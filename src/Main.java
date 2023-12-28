663a7ba3f1dc5dbaf378d5f62c65fd9ed2b30516
Arhizmei <arhizmei@gmail.com>
2023-12-28 12:49:11 +0200

    succues

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.List;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Kata {
+
+    public static List<Object> filterList(final List<Object> list) {
+        List<Object> intList = new ArrayList<>();
+        for(int i=0; i < list.size(); i++){
+            if(list.get(i) instanceof Integer) intList.add(list.get(i));
+        } return intList;
     }
 }
